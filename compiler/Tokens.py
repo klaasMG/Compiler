0a0005b86025ec2fe03eade6a22e47d4565d2277
klaasMG <klaasyoutube02@outlook.com>
2026-03-16 23:45:36 +0100

    Build the tokeniser

---
+++ compiler/Tokens.py
@@ -0,0 +1,22 @@
+from enum import Enum
+from dataclasses import dataclass
+
+class Keyword(Enum):
+    Int = "int"
+    Empty = 0
+
+
+class TokenType(Enum):
+    Keyword = "keyword"
+    Identifier = "identifier"
+    Plus = "+"
+    Minus = "-"
+    Multiply = "*"
+    Divide = "/"
+    Equal = "="
+    Empty = 0
+    
+@dataclass
+class Token:
+    TokenType: TokenType
+    value: str | Keyword
